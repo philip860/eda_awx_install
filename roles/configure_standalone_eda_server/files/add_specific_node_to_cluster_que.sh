46115676a38927f8e919fb8f635a687797e8a1bf
Duncan, Philip <philip.duncan@uconn.edu>
2024-08-01 00:47:19 -0400

    update

--- roles/configure_standalone_eda_server/files/add_specific_node_to_cluster_que.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/awx-manage provision_instance --hostname $HOSTNAME --node_type control
-
-/usr/bin/awx-manage register_queue --queuename cluster_name --hostname $HOSTNAME
--- roles/configure_standalone_eda_server/files/backup_super_user_creation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/awx-manage createsuperuser --username backup_username --no-input --email iam@uconn.edu && /usr/bin/awx-manage update_password --username=backup_username --password=backup_password && exit
